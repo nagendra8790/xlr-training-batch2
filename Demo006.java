9cac8a884d77fee8354b24ec7181b8e0d1ed544b
nagendra8790 <2311cs010704@mallareddyuniversity.ac.in>
2025-01-03 10:20:24 +0530

    Add files via upload

---
+++ Demo006.java
@@ -0,0 +1,10 @@
+public class Demo006 {
+    public static void main(String[] args) {
+        String username = "Nagendra";
+        String pd = "asdfasdf";        
+        while ((username == "Nagendra") && (pd == "asdfasdf")) { 
+            System.out.println("hello Nagendra have a nice Day");
+            break;
+        }
+    }
+}
