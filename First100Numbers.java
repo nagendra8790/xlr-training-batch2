c9524a095a583e5e51ae10d5250747b88cd2825d
nagendra8790 <2311cs010704@mallareddyuniversity.ac.in>
2025-01-06 19:53:03 +0530

    Update First100Numbers.java

--- First100Numbers.java
+++ First100Numbers.java
@@ -1,6 +1,5 @@
 public class First100Numbers {
     public static void main(String[] args) {
-        // Loop from 1 to 100
         for (int i = 1; i <= 100; i++) {
             System.out.println(i);
         }
