2d754ce88fb8cdf4082d6053c0268c055b58bb1c
nagendra8790 <2311cs010704@mallareddyuniversity.ac.in>
2025-01-06 19:51:59 +0530

    Update GreatestOfThreeNumbers.java

--- GreatestOfThreeNumbers.java
+++ GreatestOfThreeNumbers.java
@@ -1,11 +1,9 @@
 public class GreatestOfThreeNumbers {
     public static void main(String[] args) {
-        // Assign the numbers
         int num1 = 10;
         int num2 = 20;
         int num3 = 15;
 
-        // Determine the greatest number
         int greatest;
         if (num1 >= num2 && num1 >= num3) {
             greatest = num1;
@@ -15,7 +13,7 @@ public class GreatestOfThreeNumbers {
             greatest = num3;
         }
 
-        // Output the result
+    
         System.out.println("The greatest number is: " + greatest);
     }
 }
