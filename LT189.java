465ffbae6a78c514035c5255caef0b44421ed84d
nagendra8790 <2311cs010704@mallareddyuniversity.ac.in>
2025-02-27 09:45:19 +0530

    Add files via upload

---
+++ LT189.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int len =nums.length;
+        k=k%len;
+        int[] rotated=new int[len];
+
+        for(int i=0;i<len;i++){
+            rotated[(k+i)%len] = nums[i];
+        }
+        for(int i=0;i<len;i++){
+            nums[i]=rotated[i];
+        }
+    }
+}
